b241b4353e2de0da41a901466da50816f79d95b0
emeraldloop <sergey.nikishin.2000@mail.ru>
2021-11-06 19:00:52 +0300

    Добавьте файлы проекта.

---
+++ LR3_med_filter/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace LR3_med_filter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(147, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Загрузить изображение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выходToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(850, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(528, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Фильтр";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(31, 115);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(790, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(338, 48);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 48);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Сохранить изображение";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 541);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ LR3_med_filter/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LR3_med_filter
+{
+    public partial class Form1 : Form
+    {
+        private Image image;
+        Bitmap image1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // загрузка изображения
+        {
+            openFileDialog1.Filter = "Файлы изображений|*.bmp";
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = openFileDialog1.FileName;
+            // читаем файл в объект класса Image
+            try
+            {
+                image = Image.FromFile(filename);
+                pictureBox1.Image = image;
+                MessageBox.Show("Файл открыт","Оповещение");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка чтения картинки"+ex);
+                return;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) // сохранение изображения
+        {
+            if (image != null)
+            {
+                saveFileDialog1.Filter = "Файлы изображений|*.bmp";
+                saveFileDialog1.ShowDialog();
+                if (saveFileDialog1.FileName != "")
+                {
+                    saveFileDialog1.FileName = saveFileDialog1.FileName.ToUpper(); // название в верхнем регистре
+                    System.IO.FileStream fs = (System.IO.FileStream)saveFileDialog1.OpenFile();
+                    image.Save(fs, System.Drawing.Imaging.ImageFormat.Bmp);
+                    fs.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Нет изображения", "Ошибка");
+                return;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) // фильтрация
+        {
+            image1 = new Bitmap(image);
+            Bitmap window;
+            Rectangle rectangle;
+            for(int i=1;i<image1.Height-1;i++)
+            {
+                for(int j=1;j<image1.Width-1;j++)
+                {
+                    rectangle = new Rectangle(j-1,i-1,3,3);
+                    window = image1.Clone(rectangle, 0);
+                    int R = getMedian(window, "R");
+                    int G = getMedian(window, "G");
+                    int B = getMedian(window, "B");
+                    var set = Color.FromArgb(R, G, B); //Red, Green, Blue
+                    image1.SetPixel(j, i, set);
+
+                }
+            }
+            image = image1;
+            pictureBox1.Image = image;
+
+        }
+        private int getMedian(Bitmap window,string mode)
+        {
+            int[] array = new int[9];
+            int c = 0;
+            switch(mode)
+            {
+                case "R":
+                    for (int i = 0; i < 3; i++)//заполняем одномерный массив R значениями пискселей
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            array[c] = Convert.ToInt32(window.GetPixel(j, i).R);
+                            c++;
+                        }
+                    }
+                    int s = array.Length;
+                    for (int i=0;i<array.Length;i++) // сортируем массив по возрастанию
+                    {                        
+                        for(int j=0;j<s-1;j++)
+                        {
+                            if(array[j]>array[j+1])
+                            {
+                                int k = array[j];
+                                array[j] = array[j + 1];
+                                array[j + 1] = k;
+                            }
+                        }
+                        s--;
+                    }
+                    return array[4];
+                case "G":
+                    for (int i = 0; i < 3; i++)//заполняем одномерный массив R значениями пискселей
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            array[c] = Convert.ToByte(window.GetPixel(j, i).G);
+                            c++;
+                        }
+                    }
+                    s = array.Length;
+                    for (int i = 0; i < array.Length; i++) // сортируем массив по возрастанию
+                    {
+                        for (int j = 0; j < s-1; j++)
+                        {
+                            if (array[j] > array[j + 1])
+                            {
+                                int k = array[j];
+                                array[j] = array[j + 1];
+                                array[j + 1] = k;
+                            }
+                        }
+                        s--;
+                    }
+                    return array[4];
+                case "B":
+                    for (int i = 0; i < 3; i++)//заполняем одномерный массив R значениями пискселей
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            array[c] = Convert.ToByte(window.GetPixel(j, i).B);
+                            c++;
+                        }
+                    }
+                    s = array.Length;
+                    for (int i = 0; i < array.Length; i++) // сортируем массив по возрастанию
+                    {
+                        for (int j = 0; j < s-1; j++)
+                        {
+                            if (array[j] > array[j + 1])
+                            {
+                                int k = array[j];
+                                array[j] = array[j + 1];
+                                array[j + 1] = k;
+                            }
+                        }
+                        s--;
+                    }
+                    return array[4];
+                default:
+                    MessageBox.Show("Ошибка", "Ошибка");
+                    throw new NullReferenceException();
+
+            }
+            
+        }
+    }
+}
